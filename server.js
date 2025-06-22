007dccfded5911fdf63c41d7cd3da3681527d7c4
Gustavo09876 <gustavovinicius1379@gmail.com>
2025-06-22 00:27:41 -0400

    backend

--- server.js
+++ server.js
@@ -1,19 +1,57 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
 
 const users = []
 
-app.post('/usuarios', (req, res) => {
+app.post('/usuarios', async (req, res) => {
 
-    users.push(req.body)
+    await prisma.User.create({
+        data: {
+            email: req.body.email,
+            password: req.body.password
+        }
+    })
 
     res.status(201).json(req.body)
 })
 
-app.get('/usuarios', (req, res)  => {
+app.put('/usuarios/:id', async (req, res) => {
+
+    await prisma.User.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            password: req.body.password
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+app.get('/usuarios', async (req, res)  => {
+
+    const users = await prisma.User.findMany()
     res.status(200).json(users)
 })
+
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.User.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(204).send()
+})
+
+
 
 app.listen(3000)
